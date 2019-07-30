4c0d98446fd32b13cc88456a28c0075d7f437c2e
Gabriel Sawich <gsawich@gmail.com>
2019-07-30 13:41:07 -0600

    SendInBlue integration

--- alvari-front/src/App.js
+++ alvari-front/src/App.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import { Form, Button } from 'react-bootstrap'
 import logo from './alvari.svg';
 import './App.css'
 import './bootstrap/css/bootstrap.min.css'
 
+const apiKey = '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'
+
 function App() {
+  const [bText, setText] = useState('Get Updates')
+  const emailRef = useRef(null)
   return (
     <div className="App">
       <header className="App-header" style={{backgroundColor:'#110039'}}>
@@ -12,18 +16,36 @@ function App() {
         <h3>
           Cloud-based Evolutionary Digital Asset Trading
         </h3>
-        <h2>Coming Fall 2019</h2>
+        <h1>Coming Q4 2019</h1>
+        <div>
         <br />
+        <br />
+        </div>
         <Form>
           <Form.Group controlId="formBasicEmail">
             <Form.Label>Stay up-to-date by joining our mailing list!</Form.Label>
-            <Form.Control type="email" placeholder="Your email"/>
+            <Form.Control type="email" ref={emailRef} placeholder="Your email"/>
             <Form.Text className="text-muted">
               Your email is completely confidential.
             </Form.Text>
           </Form.Group>
-          <Button variant="primary" type="submit">
-            Get Updates
+          <Button variant="primary" onClick={ e=> {
+            if (bText === 'Get Updates') {
+              setText('Sending...')
+              let url = 'https://api.sendinblue.com/v3/contacts'
+              let content = { "email":emailRef.current.value }
+              fetch(url, { method:'POST', headers: { 'content-type': 'application/json', 'api-key': apiKey }, body: JSON.stringify(content) } ).then( (data) => {
+                data.json()
+                console.log(data)
+                if (data.status < 400) {
+                  setText('Signed Up!')
+                } else {
+                  setText('Failed to sign up! :(')
+                }
+              })
+            }
+          }}>
+            {bText}
           </Button>
         </Form>
       </header>
